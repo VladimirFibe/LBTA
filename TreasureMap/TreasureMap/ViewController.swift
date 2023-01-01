8e6a581fcf172116199da1821664f495bf72e015
Vladimir Fibe <motiw@icloud.com>
2023-01-01 12:03:09 +0600

    add SwiftUI Preview

--- TreasureMap/TreasureMap/ViewController.swift
+++ TreasureMap/TreasureMap/ViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 import MapKit
+import LBTATools
+import SwiftUI
 
 class ViewController: UIViewController {
 
@@ -12,14 +14,32 @@ class ViewController: UIViewController {
 
     func setupMap() {
         view.addSubview(mapView)
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            mapView.topAnchor.constraint(equalTo: view.topAnchor),
-            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+        mapView.mapType = .standard
+        mapView.fillSuperview()
+        setupRegionForMap()
     }
-
+    
+    func setupRegionForMap() {
+        let coordinateNewYork = CLLocationCoordinate2D(latitude: 40.783466, longitude: -73.971266)
+        let coordinateSanFrancisco = CLLocationCoordinate2D(latitude: 37.766610, longitude: -122.427290)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinateSanFrancisco, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 }
 
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ViewController {
+        ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+            .ignoresSafeArea()
+    }
+}
