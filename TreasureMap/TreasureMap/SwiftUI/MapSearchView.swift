18f4e1c5abf331ea55e9493878ff722f838392ca
fibeapp <motiw@me.com>
2023-01-05 05:12:03 +0600

    28. TextField Autosearching with Combine

--- TreasureMap/TreasureMap/SwiftUI/MapSearchView.swift
+++ TreasureMap/TreasureMap/SwiftUI/MapSearchView.swift
@@ -3,31 +3,44 @@ import MapKit
 
 struct MapSearchView: View {
     @ObservedObject var viewModel = MapSearchViewModel()
+   
     var body: some View {
         ZStack(alignment: .top) {
             MapViewContainer(annotations: viewModel.annotations)
                 .ignoresSafeArea()
-            HStack {
-                Button(action: {
-                    viewModel.performLocalSearch("sushi")
-                }) {
-                    Text("Search for Airports")
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 50)
-                        .background(Color.white)
-                }
-                Button(action: {
-                    viewModel.removeLocations()
-                }) {
-                    Text("Search for Airports")
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 50)
-                        .background(Color.white)
-                }
+            VStack {
+                TextField("Search term", text: $viewModel.searchQuery)
+                    .padding(12)
+                    .background(Color.white)
+                    .padding()
+                .shadow(radius: 3)
+                Text(viewModel.isSearching ? "Searching..." : "")
+                    .foregroundColor(.black)
+                    .shadow(color: .white, radius: 2)
             }
-            .shadow(radius: 3)
-            .padding()
         }
+    }
+    var buttons: some View {
+        HStack {
+            Button(action: {
+                viewModel.performLocalSearch("sushi")
+            }) {
+                Text("Search for Airports")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.white)
+            }
+            Button(action: {
+                viewModel.removeLocations()
+            }) {
+                Text("Search for Airports")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.white)
+            }
+        }
+        .shadow(radius: 3)
+        .padding()
     }
 }
 
--- TreasureMap/TreasureMap/SwiftUI/MapSearchViewModel.swift
+++ TreasureMap/TreasureMap/SwiftUI/MapSearchViewModel.swift
@@ -1,19 +1,37 @@
 import SwiftUI
 import MapKit
+import Combine
 
 final class MapSearchViewModel: ObservableObject {
     @Published var annotations: [MKPointAnnotation] = []
+    @Published var isSearching = false
+    @Published var searchQuery = ""
     
+    private var bag = Set<AnyCancellable>()
+    
+    init() {
+        $searchQuery
+//            .dropFirst()
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .sink {[weak self] searchTerm in
+                guard let self = self else { return }
+                self.performLocalSearch(searchTerm)
+            }
+            .store(in: &bag)
+    }
     func removeLocations() {
         annotations = []
     }
     
     func performLocalSearch(_ text: String) {
+        guard !text.isEmpty else {return}
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = text
         let localSearch = MKLocalSearch(request: request)
+        isSearching = true
         localSearch.start { response, error in
             if let error = error {
+                self.isSearching = false
                 print("DEBUG: \(error.localizedDescription)")
                 return
             }
@@ -27,6 +45,7 @@ final class MapSearchViewModel: ObservableObject {
                 airports.append(annotation)
             })
             self.annotations = airports
+            self.isSearching = false
         }
     }
 }
